caca3aba9209308174287c8dd701a6220b321030
DT0998 <duongkytrieuvn@gmail.com>
2024-04-11 17:03:03 +0700

    feat: clean up

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { templateCompilerOptions } from '@tresjs/core'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueJsx(),
+    vue({
+      // Other config
+      ...templateCompilerOptions
+    }),
+    vueJsx()
   ],
   resolve: {
     alias: {
